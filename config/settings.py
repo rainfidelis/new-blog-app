90b7034a859e730363f28dbb2bf79e7c07c77fcb
rainfidelis <rainnyfidelis@yahoo.com>
2021-09-07 14:20:02 +0100

    heroku db config

--- config/settings.py
+++ config/settings.py
@@ -89,14 +89,17 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': BASE_DIR / 'db.sqlite3',
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('DB_NAME'),
-        'HOST': config('HOST'),
-        'PORT': config('PORT'),
-        'USER': config('DB_USER'),
-        'PASSWORD': config('DB_PASSWORD')
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+
+        # Postgres Config for Local Machine
+
+        # 'ENGINE': 'django.db.backends.postgresql',
+        # 'NAME': config('DB_NAME'),
+        # 'HOST': config('HOST'),
+        # 'PORT': config('PORT'),
+        # 'USER': config('DB_USER'),
+        # 'PASSWORD': config('DB_PASSWORD')
     }
 }
 # Password validation
